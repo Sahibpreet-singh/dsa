ce4c8bde9c5ebf3f7d40171aa285b9b1913cf9e6
Sahibpreet-singh <165685049+Sahibpreet-singh@users.noreply.github.com>
2026-06-17 23:43:44 +0530

    gcd in strings

---
+++ strings/gcd_strings.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std;
+#include <bits/stdc.h++>
+
+int ans(string str1,string str2){
+
+}
+
+int main() {
+	string str1;
+	string str2;
+	cin>>str1;
+	cin>>str2;
+	
+	
+	
+	return 0;
+}
