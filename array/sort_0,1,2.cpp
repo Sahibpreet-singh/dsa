258468a390846c1a95d33cfc0e71995164660db9
Sahibpreet-singh <165685049+Sahibpreet-singh@users.noreply.github.com>
2026-06-17 23:39:50 +0530

    updated

--- array/sort_0,1,2.cpp
+++ array/sort_0,1,2.cpp
@@ -1,51 +1,41 @@
-#include<bits/stdc++.h>
+#include <iostream> 
+#include <bits/stdc++.h>
 using namespace std;
+void s012(vector<int> &arr,int n){
+	int low,mid,high;
+	low=0;high=n-1;
+	mid=0;
+	while(mid<=high){
+		if(arr[mid]==0){
+			swap(arr[low],arr[mid]);
+			mid++;
+			low++;
+		}
+		else if(arr[mid]==1){
+			mid++;
+		}
+		else{
+			swap(arr[mid],arr[high]);
+			high--;
+		}
+	}
 
-class Solution {
-public:
-    // Function to sort the array containing only 0s, 1s, and 2s
-    void sortZeroOneTwo(vector<int>& nums) {
-        
-        // 3 pointers: low, mid, high
-        int low = 0, mid = 0, high = nums.size() - 1; 
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                
-                /* Swap nums[low] and nums[mid], then 
-                 move both low and mid pointers forward*/
-                swap(nums[low], nums[mid]);
-                low++;
-                mid++;
-                
-            }
-            else if (nums[mid] == 1) {
-                // Move mid pointer forward
-                mid++;
-            }
-            else {
-                /* Swap nums[mid] and nums[high], 
-                then move high pointer backward*/
-                swap(nums[mid], nums[high]);
-                high--;
-            }
-        }
-    }
-};
+}
 
 int main() {
-    vector<int> nums = {0, 2, 1, 2, 0, 1};
-    
-    // Create an instance of Solution class
-    Solution sol;
+	int n;
+	cin>>n;
+	vector<int>arr(n);
+	
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	
+	s012(arr,n);
+	
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
 
-    sol.sortZeroOneTwo(nums);
-    
-    // Print the array elements after sorting
-    cout << "After sorting:" << endl;
-    for (int i = 0; i < nums.size(); i++) {
-        cout << nums[i] << " ";
-    }
-    cout << endl;
-    
-    return 0;
+	return 0;
 }
