934de117b41de27da0933d0d796cdab624b970da
Sahibpreet-singh <165685049+Sahibpreet-singh@users.noreply.github.com>
2026-06-22 21:18:49 +0530

    recursion

---
+++ recursion/frogjump.cpp
@@ -0,0 +1,30 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+//[2,4,5,-1,-3,-4]
+int f(vector<int>arr,int n){
+    if(n<=0)return 0;
+   
+    int take=f(arr,n-1)+abs(arr[n]-arr[n-1]);
+   int ntake;
+    if(n>1){
+        ntake=f(arr,n-2)+abs(arr[n]-arr[n-2]);
+    }
+    return min(take,ntake);
+        
+}
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    cout<<f(arr,n-1);  
+    
+    return 0;
+}
