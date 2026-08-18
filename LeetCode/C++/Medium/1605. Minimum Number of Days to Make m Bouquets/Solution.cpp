16af68746428341a33a2137355c7519a8175c56a
Sahibpreet Singh <sahibpreetrai26@gmail.com>
2026-08-18 23:02:24 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/1605. Minimum Number of Days to Make m Bouquets/Solution.cpp
+++ LeetCode/C++/Medium/1605. Minimum Number of Days to Make m Bouquets/Solution.cpp
@@ -1,42 +1,61 @@
 class Solution {
 public:
-  bool func(vector<int>arr,int n,int days,int k,int m){
-    int noob=0;int cnt=0;
-    for(int i=0;i<n;i++){
-        if(arr[i]<=days){
-            cnt++;
-        }
-        else{
-            noob+=cnt/k;
-            cnt=0;
-        }
-
-    }
-    noob+=cnt/k;
-    return noob>=m;
-  }
-  
-  
-    int minDays(vector<int>& arr, int m, int k) {
-      long long val = m * 1ll * k * 1ll;
-     //size of the array
-        int n=arr.size();
-    if (val > n) return -1;
-        int low=*min_element(arr.begin(),arr.end());
-        int high=*max_element(arr.begin(),arr.end());
-       while(low<=high){
-        int mid=(low+high)/2;
-        if(func(arr,n,mid,k,m)){
-            high=mid-1;
-        }
-        else{
-            low=mid+1;
-        }
-
-       }
+    bool possible(vector<int> &nums, int day, int m, int k) {
+        int n = nums.size(); 
         
-    return low;
+        // Count of flowers bloomed
+        int cnt = 0; 
+        
+        // Count of bouquets formed
+        int noOfB = 0; 
 
+        // Count number of bouquets that can be formed
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= day) {
+                // Increment flower count
+                cnt++; 
+            } else {
+                /* Calculate number of bouquets
+                formed with flowers <= day */
+                noOfB += (cnt / k);
+                
+                // Reset flower count
+                cnt = 0; 
+            }
+        }
+        // Add remaining flowers as a bouquet
+        noOfB += (cnt / k); 
 
+        /* Return true if enough 
+        bouquets can be formed */
+        return noOfB >= m; 
+    }
+   
+    int minDays(vector<int>& nums, int m, int k) {
+        int n=nums.size();
+        long long val = m * 1ll * k * 1ll; 
+        
+        if (val > n) return -1; 
+        
+        int mini = INT_MAX, maxi = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            mini = min(mini, nums[i]); 
+            maxi = max(maxi, nums[i]); 
+        }
+        
+        int left = mini, right = maxi, ans = -1;
+        while (left <= right) {
+            
+            int mid = left + (right - left) / 2; 
+            
+            if (possible(nums, mid, m, k)) {
+                 
+                
+                right = mid - 1; 
+            } else {
+                left = mid + 1; 
+            }
+        }
+        return left; 
     }
 };
