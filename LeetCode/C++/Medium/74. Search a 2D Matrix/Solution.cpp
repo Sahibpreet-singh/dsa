caef8b2f9a1694c7f4161bf554f7a29799a16cf7
Sahibpreet Singh <sahibpreetrai26@gmail.com>
2026-08-23 10:15:47 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/74. Search a 2D Matrix/Solution.cpp
+++ LeetCode/C++/Medium/74. Search a 2D Matrix/Solution.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& arr, int target) {
-        int n=arr.size();
+         int n=arr.size();
         int m=arr[0].size();
-        int low=0;
-        int high=n*m-1;
-        
+
+        int low=0,high=n*m-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            int rows=mid/m;
-            int colm=mid%m;
-            if(arr[rows][colm]==target){return true;}
-            else if(arr[rows][colm]<target)low=mid+1;
+            int mid=low+(high-low)/2;
+            int row=mid/m;
+            int col=mid%m;
+            if(arr[row][col]==target){
+                return true;
+            }
+            else if(arr[row][col]<target){
+                low=mid+1;
+            }
             else high=mid-1;
-
+       
         }
-
-    return false;
+        
+        return false;
     }
 };
