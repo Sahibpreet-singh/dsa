5542fbe4d63ba62f37cd7f7e2bd68421b7e07f73
Sahibpreet Singh <sahibpreetrai26@gmail.com>
2026-09-01 23:07:10 +0530

    Add Medium C++ Solution for 3870. Minimum Moves to Clean the Classroom from LeetCode

---
+++ LeetCode/Patterns/Matrix/3870. Minimum Moves to Clean the Classroom/Solution.cpp
@@ -0,0 +1,108 @@
+class Solution {
+public:
+    int minMoves(vector<string>& classroom, int energy) {
+        int m = classroom.size();
+        int n = classroom[0].size();
+
+        int sr, sc;
+        vector<vector<int>> id(m, vector<int>(n, -1));
+
+        int k = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (classroom[i][j] == 'S') {
+                    sr = i;
+                    sc = j;
+                }
+
+                if (classroom[i][j] == 'L') {
+                    id[i][j] = k++;
+                }
+            }
+        }
+
+        if (k == 0)
+            return 0;
+
+        int fullMask = (1 << k) - 1;
+
+        // best[r][c][mask] = maximum energy reached
+        vector<vector<vector<int>>> best(
+            m,
+            vector<vector<int>>(n, vector<int>(1 << k, -1))
+        );
+
+        queue<tuple<int, int, int, int>> q;
+
+        // r, c, mask, energy
+        q.push({sr, sc, 0, energy});
+        best[sr][sc][0] = energy;
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        int moves = 0;
+
+        while (!q.empty()) {
+
+            int sz = q.size();
+
+            while (sz--) {
+
+                auto [r, c, mask, en] = q.front();
+                q.pop();
+
+                if (mask == fullMask)
+                    return moves;
+
+                if (en == 0)
+                    continue;
+
+                for (int d = 0; d < 4; d++) {
+
+                    int nr = r + dr[d];
+                    int nc = c + dc[d];
+
+                    if (nr < 0 || nr >= m ||
+                        nc < 0 || nc >= n)
+                        continue;
+
+                    if (classroom[nr][nc] == 'X')
+                        continue;
+
+                    int newEnergy = en - 1;
+                    int newMask = mask;
+
+                    // Collect litter
+                    if (classroom[nr][nc] == 'L') {
+                        newMask |= (1 << id[nr][nc]);
+                    }
+
+                    // Reset
+                    if (classroom[nr][nc] == 'R') {
+                        newEnergy = energy;
+                    }
+
+                    // Already reached with >= energy
+                    if (best[nr][nc][newMask] >= newEnergy)
+                        continue;
+
+                    best[nr][nc][newMask] = newEnergy;
+
+                    q.push({
+                        nr,
+                        nc,
+                        newMask,
+                        newEnergy
+                    });
+                }
+            }
+
+            moves++;
+        }
+
+        return -1;
+    }
+};
