9ea0bc63eef485e23c73bb585fd772b2f9b4cdea
Sahibpreet Singh <sahibpreetrai26@gmail.com>
2026-09-06 20:46:18 +0530

    Add Hard C++ Solution for 115. Distinct Subsequences from LeetCode

--- LeetCode/Patterns/Dynamic Programming/115. Distinct Subsequences/Solution.cpp
+++ LeetCode/Patterns/Dynamic Programming/115. Distinct Subsequences/Solution.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int f(string &s,string &t,int i,int j,vect){
+    int f(string &s,string &t,int i,int j,vector<vector<int>>&dp){
         if(j==t.size()) return 1;
         if(i==s.size()) return 0;
-
+        if (dp[i][j] != -1) return dp[i][j];
+        
         if(s[i]==t[j]){
-            int take=f(s,t,i+1,j+1);
-            int ntake=f(s,t,i+1,j);
-            return take+ntake;
+            int take=f(s,t,i+1,j+1,dp);
+            int ntake=f(s,t,i+1,j,dp);
+            return dp[i][j]=take+ntake;
         }
-        return f(s,t,i+1,j);
+        return dp[i][j]=f(s,t,i+1,j,dp);
     }
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(s,t,0,0);
+        return f(s,t,0,0,dp);
     }
 };
