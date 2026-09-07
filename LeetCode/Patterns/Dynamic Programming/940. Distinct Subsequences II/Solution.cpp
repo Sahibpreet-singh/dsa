0f38ddda6ef9d260ef01775007708c7da79c1eab
Sahibpreet Singh <sahibpreetrai26@gmail.com>
2026-09-07 19:51:44 +0530

    Add Hard C++ Solution for 940. Distinct Subsequences II from LeetCode

---
+++ LeetCode/Patterns/Dynamic Programming/940. Distinct Subsequences II/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void f(string &s,int i,string current,set<string>&st){
+        if(i==s.size()){
+            if(!current.empty()){
+                st.insert(current);
+            }
+            return;
+        }
+        
+        f(s,i+1,current+s[i],st);
+        f(s,i+1,current,st);
+
+    }
+    int distinctSubseqII(string s) {
+        set<string> st;
+        f(s,0,"",st);   
+        return st.size();
+    }
+};
