245f2b2fbc5d36608d8197d3ca0fccd44ab2a142
Sahibpreet Singh <sahibpreetrai26@gmail.com>
2026-09-11 11:50:45 +0530

    Add Easy C++ Solution for 3799. Unique 3-Digit Even Numbers from LeetCode

--- LeetCode/C++/Easy/3799. Unique 3-Digit Even Numbers/Solution.cpp
+++ LeetCode/C++/Easy/3799. Unique 3-Digit Even Numbers/Solution.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        sort(digits.begin(), digits.end());
-        int ans = 0;
-        do {
-            if (digits[0] == 0)
-                continue;
-            if (digits[2] % 2 == 0)
-                ans++; 
-        } while (next_permutation(digits.begin(), digits.end()));
+        vector<int> freq(10, 0);
 
-        return ans;
+        for (int d : digits) {
+            freq[d]++;
+        }
+
+        int count = 0;
+
+        for (int num = 100; num <= 999; num += 2) {
+            int ones = num % 10;
+            int tens = (num / 10) % 10;
+            int hundreds = num / 100;
+
+            freq[hundreds]--;
+            freq[tens]--;
+            freq[ones]--;
+
+            if (freq[hundreds] >= 0 &&
+                freq[tens] >= 0 &&
+                freq[ones] >= 0) {
+                count++;
+            }
+
+            freq[hundreds]++;
+            freq[tens]++;
+            freq[ones]++;
+        }
+
+        return count;
     }
 };
