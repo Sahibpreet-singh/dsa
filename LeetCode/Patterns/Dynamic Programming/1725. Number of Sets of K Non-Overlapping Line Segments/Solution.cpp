8b6d1c6be1acae13ad3b9bee3825ca273a745c70
Sahibpreet Singh <sahibpreetrai26@gmail.com>
2026-09-16 10:57:23 +0530

    Add Medium C++ Solution for 1725. Number of Sets of K Non-Overlapping Line Segments from LeetCode

--- LeetCode/Patterns/Dynamic Programming/1725. Number of Sets of K Non-Overlapping Line Segments/Solution.cpp
+++ LeetCode/Patterns/Dynamic Programming/1725. Number of Sets of K Non-Overlapping Line Segments/Solution.cpp
@@ -2,36 +2,40 @@ class Solution {
 public:
     int MOD = 1e9 + 7;
 
-    long long f(int i, int k, vector<vector<long long>>& dp) {
-
-        // No segments left
-        if (k == 0)
-            return 1;
-
-        // Not enough points for another segment
-        if (i < 1)
-            return 0;
-
-        if (dp[i][k] != -1)
-            return dp[i][k];
-
-        // Don't use point i
-        long long ans = f(i - 1, k, dp);
-
-        // Make a segment ending at i
-        for (int j = 0; j < i; j++) {
-            ans += f(j, k - 1, dp);
-            ans %= MOD;
-        }
-
-        return dp[i][k] = ans;
-    }
-
     int numberOfSets(int n, int k) {
+
         vector<vector<long long>> dp(
-            n, vector<long long>(k + 1, -1)
+            n, vector<long long>(k + 1, 0)
         );
 
-        return f(n - 1, k, dp);
+        vector<vector<long long>> sum(
+            n, vector<long long>(k + 1, 0)
+        );
+
+        // 0 segments = 1 way for every i
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+            sum[i][0] = i + 1;
+        }
+
+        for (int seg = 1; seg <= k; seg++) {
+
+            for (int i = 1; i < n; i++) {
+
+                // Don't use point i
+                dp[i][seg] = dp[i - 1][seg];
+
+                // Make a segment ending at i
+                dp[i][seg] += sum[i - 1][seg - 1];
+
+                dp[i][seg] %= MOD;
+
+                // Prefix sum
+                sum[i][seg] =
+                    (sum[i - 1][seg] + dp[i][seg]) % MOD;
+            }
+        }
+
+        return dp[n - 1][k];
     }
 };
